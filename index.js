1ee3ed81ab96b157a3d5660bf07ac2622a8b2373
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-02-09 15:16:14 +0545

    Add chessboard for  knight-travail

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import { Travail } from "./travail.js";
 
-let grid = [
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0]
-];
+function chessBoard() {
+    const res = [];
+    for (let i = 0; i < 8; i++) {
+        res.push([]);
+        for (let j = 0; j < 8; j++) {
+            res[i].push(( i + j ) % 2 === 0 ? 1 : 0);
+        }
+    }
+    return res;
+}
+
+
+let grid = chessBoard();
+const source = [0, 0];
+const destination = [7, 7];
+
+const knightTravail = new Travail(grid, source, destination);
+const path = knightTravail.travail();
+console.log(path);
